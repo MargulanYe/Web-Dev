92110237393eb535bb460ffe035a8b0fe9cc8be8
MargulanYe <ezhekhanmargulan@gmail.com>
2026-02-24 21:48:28 +0500

    Create categories.ts

---
+++ Lab5/data/categories.ts
@@ -0,0 +1,8 @@
+import { Category } from '../models/category.model';
+
+export const CATEGORIES: Category[] = [
+  { id: 1, name: 'Smartphones' },
+  { id: 2, name: 'Wearables' },
+  { id: 3, name: 'Computers' },
+  { id: 4, name: 'Accessories' }
+];
